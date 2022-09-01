c295b239b759a8458ebb867a37cbd2af104f053f
ilya <123ilya@gmail.com>
2022-09-01 23:44:59 +1000

    обавил функцию, добавляющую в массив элементы

--- index.php
+++ index.php
@@ -1,2 +1,16 @@
 <?php
-$textStorage;
+
+// 1
+$textStorage = [];
+//2-------------------------------
+function add($title, $text, &$array)
+{
+ $subArray             = [];
+ $subArray[0]['title'] = $title;
+ $subArray[0]['text']  = $text;
+ array_push($array, $subArray);
+}
+
+add('djope','ojkjhkjhkjhkjhkjh',$textStorage);
+add('писке','гдппалрпаорпаоап',$textStorage);
+var_dump($textStorage);
