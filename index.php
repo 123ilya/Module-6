466ca518e10f60ecc12a98be697189fe5d9b83cc
Илья А. Соболев <sobolev_ia@pippkro.ru>
2022-09-06 11:05:58 +1000

    Исправил add

--- index.php
+++ index.php
@@ -1,40 +1,39 @@
 <?php
 
 $textStorage = [];
+//-------------------------------------------------------------------------------------
 //Функция добавляет в конец масива новый элемент (ассациативный массив с полями 'title', 'text');
-function add(string $title, string $text, array&$array)
+function add(string $title, string $text, array &$array)
 {
- $subArray          = [];
- $subArray['title'] = $title;
- $subArray['text']  = $text;
- array_push($array, $subArray);
+    $article = ['title' => $title, 'text' => $text];
+    array_push($array, $article);
 }
 
 add('tttttttt', 'ojkjhkjhkjhkjhkjh', $textStorage);
 add('rrrrrrrrr', 'гдппалрпаорпаоап', $textStorage);
-// var_dump($textStorage);
-//Функция удаляет элемент из массива.
-function remove(int $number, array&$array)
-{
- if ($array[$number]) {
-  unset($array[$number]);
-  return true;
- } else {
-  return false;
- }
-}
-//-------------------------------
-function edit(int $index, string $title, string $text, array&$array)
-{
- if ($array[$index]) {
-  $array[$index]['title'] = $title;
-  $array[$index]['text']  = $text;
-  return true;
-
- } else {
-  return false;
- }
-
-}
-var_dump(edit(0, '111', '222', $textStorage));
 var_dump($textStorage);
+//------------------------------------------------------------------------------------------
+//Функция удаляет элемент из массива.
+function remove(int $number, array &$array)
+{
+    if ($array[$number]) {
+        unset($array[$number]);
+        return true;
+    } else {
+        return false;
+    }
+}
+
+//-------------------------------
+function edit(int $index, string $title, string $text, array &$array)
+{
+    if ($array[$index]) {
+        $array[$index]['title'] = $title;
+        $array[$index]['text'] = $text;
+        return true;
+    } else {
+        return false;
+    }
+}
+
+
